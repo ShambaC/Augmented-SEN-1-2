4850098041b47fc3799f051cae5492cbfb61a78c
ShambaC <38806897+ShambaC@users.noreply.github.com>
2025-03-02 10:01:50 +0530

    Feat: read coords from file
    Read coords from file and download corresponding images.
    Also create a csv file with corresponding image prompts containing region and season.

--- Dataset Tools/Copernicus Data Utils/Sen_1_Data.py
+++ Dataset Tools/Copernicus Data Utils/Sen_1_Data.py
@@ -1,110 +1,138 @@
 from AuthClient import SessionData
 from utils.calcCoords import getCoords
+from tqdm import tqdm
+
+import pandas as pd
 
 oauth, token = SessionData
 
 IW_COLLECTION_ID = 'byoc-3c662330-108b-4378-8899-525fd5a225cb'
-INITIAL_LONGITUDE = 75.41529108199907
-INITIAL_LATITUDE = 27.136261288968445
 
-coords_data = getCoords(INITIAL_LONGITUDE, INITIAL_LATITUDE)
+def saveImage(long: float, lat: float, idx: int) -> tuple[str] :
+    INITIAL_LONGITUDE = long
+    INITIAL_LATITUDE = lat
 
-# Earth engine format
-# Coords are in (longitude, latitude) format
-boxCoords = coords_data[0]
+    coords_data = getCoords(INITIAL_LONGITUDE, INITIAL_LATITUDE)
 
-########################################################################
-#                       IMAGE PROPERTIES                               #
-########################################################################
+    # Earth engine format
+    # Coords are in (longitude, latitude) format
+    boxCoords = coords_data[0]
 
-img_height = 2500
-img_width = 2500
+    ########################################################################
+    #                       IMAGE PROPERTIES                               #
+    ########################################################################
 
-bbox = coords_data[1]
+    img_height = 2500
+    img_width = 2500
 
-evalScript = """
-//VERSION=3
-function setup() {
-    return {
-        input: ["VV"],
-        output: { bands: 1 }
-    };
-}
+    bbox = coords_data[1]
 
-function evaluatePixel(sample) {
-    return [sample.VV];
-}
+    evalScript = """
+    //VERSION=3
+    function setup() {
+        return {
+            input: ["VV"],
+            output: { bands: 1 }
+        };
+    }
 
-"""
+    function evaluatePixel(sample) {
+        return [sample.VV];
+    }
 
-########################################################################
-#                    LOCATION AND SEASON DATA                          #
-########################################################################
+    """
 
-import pandas as pd
-from pathlib import Path
+    ########################################################################
+    #                    LOCATION AND SEASON DATA                          #
+    ########################################################################
 
-season_map = {}
+    import pandas as pd
+    from pathlib import Path
 
-season = "winter"
-region = ""
+    season = "winter"
+    fromDateTime = "2023-11-29T23:59:59Z"
+    toDateTime = "2024-01-01T00:00:00Z"
+    region = ""
 
-Path(f"./Images/{season}").mkdir(parents=True, exist_ok=True)
+    Path(f"./Images/{season}").mkdir(parents=True, exist_ok=True)
 
-if INITIAL_LATITUDE <= 23.5 and INITIAL_LATITUDE >= -23.5 :
-    region = "tropical"
-elif (INITIAL_LATITUDE > 23.5 and INITIAL_LATITUDE < 66.5) or (INITIAL_LATITUDE < -23.5 and INITIAL_LATITUDE > -66.5) :
-    region = "temperate"
-else :
-    region = "arctic"
+    if INITIAL_LATITUDE <= 23.5 and INITIAL_LATITUDE >= -23.5 :
+        region = "tropical"
+    elif (INITIAL_LATITUDE > 23.5 and INITIAL_LATITUDE < 66.5) or (INITIAL_LATITUDE < -23.5 and INITIAL_LATITUDE > -66.5) :
+        region = "temperate"
+    else :
+        region = "arctic"
 
-# TODO Implement looping on coords
-fileName = f"Images/{season}/img_{region}_0.png"
+    # TODO Implement looping on coords
+    fileName = f"Images/{season}/img_{region}_{idx}.png"
 
-########################################################################
-#                              REQUEST                                 #
-########################################################################
+    ########################################################################
+    #                              REQUEST                                 #
+    ########################################################################
 
-request = {
-    "input": {
-        "bounds": {
-            "bbox": bbox
-        },
-        "data": [
-            {
-                "dataFilter": {
-                    "timerange": {
-                        "from": "2023-11-29T23:59:59Z",
-                        "to": "2024-01-01T00:00:00Z"
-                    }
-                },
-                "type": IW_COLLECTION_ID
-            }
-        ]
-    },
-    "output": {
-        "width": img_width,
-        "height": img_height,
-        "responses": [
-            {
-                "identifier": "default",
-                "format": {
-                    "type": "image/png"
+    request = {
+        "input": {
+            "bounds": {
+                "bbox": bbox
+            },
+            "data": [
+                {
+                    "dataFilter": {
+                        "timerange": {
+                            "from": fromDateTime,
+                            "to": toDateTime
+                        }
+                    },
+                    "type": IW_COLLECTION_ID
                 }
-            }
-        ]
-    },
-    "evalscript": evalScript
-}
+            ]
+        },
+        "output": {
+            "width": img_width,
+            "height": img_height,
+            "responses": [
+                {
+                    "identifier": "default",
+                    "format": {
+                        "type": "image/png"
+                    }
+                }
+            ]
+        },
+        "evalscript": evalScript
+    }
 
-url = "https://sh.dataspace.copernicus.eu/api/v1/process"
-response = oauth.post(url, json=request)
+    url = "https://sh.dataspace.copernicus.eu/api/v1/process"
+    response = oauth.post(url, json=request)
 
-if response.ok :
-    print("Response OK")
-    with open(fileName, 'wb') as fp :
-        fp.write(response.content)
+    if response.ok :
+        print("Response OK")
+        with open(fileName, 'wb') as fp :
+            fp.write(response.content)
+
         print("Done saving file")
-else :
-    print(f"Response code: {response.status_code}")
-    print(response.content)
+        return (fileName, region, season)
+    else :
+        print(f"Response code: {response.status_code}")
+        print(response.content)
+        return ("error", "error", "error")
+
+
+if __name__ == "__main__" :
+    # Iterate through rows and download images
+    df = pd.read_csv("CoordsList.txt", header=None, names=['long', 'lat'])
+    prompt_dict = {}
+
+    for idx, row in tqdm(df.iterrows(), total=df.shape[0]) :
+        long = row.long
+        lat = row.lat
+
+        fileName, region, season = saveImage(long, lat, idx)
+        
+        if fileName.strip().lower() == "error" :
+            continue
+
+        prompt_dict[fileName] = f"Season: {season}, Region: {region}"
+
+    prompt_df = pd.DataFrame(prompt_dict)
+    prompt_df.to_csv("Images/prompts.csv", index=False)
