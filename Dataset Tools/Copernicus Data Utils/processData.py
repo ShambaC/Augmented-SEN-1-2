1d6d7e338833ba81a939a78e9b548e701c726fa1
ShambaC <38806897+ShambaC@users.noreply.github.com>
2025-03-06 12:13:27 +0530

    Feat: add dataset processing

--- Dataset Tools/Copernicus Data Utils/processData.py
+++ Dataset Tools/Copernicus Data Utils/processData.py
@@ -1,1 +1,47 @@
 from pathlib import Path
+from tqdm import tqdm, trange
+
+import pandas as pd
+import cv2 as cv
+
+df = pd.read_csv("./Images/prompts.csv")
+promptList = []
+
+outputFolder_s1 = Path(df.iloc[0].s1_fileName).parent.__str__()
+Path(f"../../Dataset/{outputFolder_s1}").mkdir(parents=True, exist_ok=True)
+outputFolder_s2 = Path(df.iloc[0].s2_fileName).parent.__str__()
+Path(f"../../Dataset/{outputFolder_s2}").mkdir(parents=True, exist_ok=True)
+
+k = 0
+for idx, row in tqdm(df.iterrows(), total=df.shape[0]) :
+
+    s1_image = cv.imread(f"Images/{row.s1_fileName}")
+    s2_image = cv.imread(f"Images/{row.s2_fileName}")
+
+    extracted_info = row.s1_fileName.split('/')[-1].split('_')
+    season = extracted_info[0]
+    region = extracted_info[2]
+
+    for i in trange(81) :
+
+        k += 1
+
+        x = i % 9   # Move columns
+        y = i / 9   # Move rows
+
+        crop_s1 = s1_image[256*x : 256*(x+1), 256*y : 256*(y+1)]
+        crop_s2 = s2_image[256*x : 256*(x+1), 256*y : 256*(y+1)]
+
+        cv.imwrite(f"../../Dataset/{outputFolder_s1}/{season}_img_{region}_p{k}.png", crop_s1)
+        cv.imwrite(f"../../Dataset/{outputFolder_s2}/{season}_img_{region}_p{k}.png", crop_s2)
+
+        promptList.append([
+            f"{outputFolder_s1}/{season}_img_{region}_p{k}.png",
+            f"{outputFolder_s2}/{season}_img_{region}_p{k}.png",
+            row.prompt
+            ])
+        
+prompt_df = pd.DataFrame(promptList, columns=["s1_fileName", "s2_fileName", "prompt"])
+prompt_df.to_csv("../../Dataset/winter/prompt.csv")
+
+print("DONE")
