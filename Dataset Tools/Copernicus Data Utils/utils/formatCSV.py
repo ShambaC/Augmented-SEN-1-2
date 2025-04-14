3d7a6be27ec79e34b1b8873c4460faf606fb48fd
ShambaC <38806897+ShambaC@users.noreply.github.com>
2025-04-14 16:07:10 +0530

    Add script to clean up csv

---
+++ Dataset Tools/Copernicus Data Utils/utils/formatCSV.py
@@ -0,0 +1,18 @@
+import pandas as pd
+
+season = "winter"
+number = 170
+df = pd.read_csv(f"../../../Dataset/{season}/prompts_{number}.csv")
+
+# Split prompt into individual columns
+df[['season', 'region']] = df['prompt'].str.split(', ', n=1, expand=True)
+
+# Remove prompt column
+df.drop(['prompt'], axis=1, inplace=True)
+
+# Remove labels from season and region columns
+df['season'] = df['season'].str.slice(start=8)
+df['region'] = df['region'].str.slice(start=8)
+
+# Save the file
+df.to_csv(f"../../../Dataset/{season}/prompts_{number} up.csv", index=False)
